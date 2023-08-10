6765dfac80344b52b4e329c4a2596f7bb9ba1bbe
HadeedTariq <120406067+HadeedTariq@users.noreply.github.com>
2023-08-10 15:52:10 +0500

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -30,7 +30,7 @@ function Form({ register, login }) {
     let form = new FormData(e.currentTarget)
     form = Object.fromEntries(form)
     try {
-      await axios.post(`${import.meta.env.VITE_API_URL}/user/auth`, form)
+      await axios.post(`${import.meta.env.VITE_API_URL}/user/auth`, form,{withCredentials:true})
       toast.dismiss(loading)
       const { data } = await axios.post(`${import.meta.env.VITE_API_URL}/user/singleUser`, {},{withCredentials:true})
       dispatch(loginUser(data))
